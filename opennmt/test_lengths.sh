300a8f5bc1dbbd742e4495c687cb69f8bede3375
Verna Dankers <vernadankers@gmail.com>
2021-09-13 20:30:44 +0100

    Cleaning scripts.

--- opennmt/test_lengths.sh
+++ opennmt/test_lengths.sh
@@ -11,31 +11,31 @@ rm "${MODEL_FOLDER}/seed=${1}_wiktionary/length" -r
 mkdir "${MODEL_FOLDER}/seed=${1}_wiktionary/length"
 
 sum=17975
-number=719
-for step in 25
-do
-    #sum=$((sum + number))
+step=25
 
-    # Nonce word with neuter tag
-    python translate.py -src "../wiktionary/s_length.src" -tgt "../wiktionary/s_length_lieblings.src" \
-                        -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
-                        -output "${PRD_FILENAME}_length_${step}.prd" --seed 1
-    wait
-    python translate.py -src "../wiktionary/s_length_haupt.src" -tgt "../wiktionary/s_length_lieblings.src" \
-                        -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
-                        -output "${PRD_FILENAME}_length_haupt_${step}.prd" --seed 1
-    wait
-    python translate.py -src "../wiktionary/s_length_not.src" -tgt "../wiktionary/s_length_lieblings.src" \
-                        -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
-                        -output "${PRD_FILENAME}_length_not_${step}.prd" --seed 1
-    wait
-    python translate.py -src "../wiktionary/s_length_see.src" -tgt "../wiktionary/s_length_lieblings.src" \
-                        -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
-                        -output "${PRD_FILENAME}_length_see_${step}.prd" --seed 1
-    wait
-    python translate.py -src "../wiktionary/s_length_lieblings.src" -tgt "../wiktionary/s_length_lieblings.src" \
-                        -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
-                        -output "${PRD_FILENAME}_length_lieblings_${step}.prd" --seed 1
-    wait
-
-done
+# The -tgt does not matter, we're only interested in the translations that will be stored to file
+# Get translations for the 'regular' inputs
+python translate.py -src "../wiktionary/s_length.src" -tgt "../wiktionary/s_length_lieblings.src" \
+                    -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
+                    -output "${PRD_FILENAME}_length_${step}.prd" --seed 1
+wait
+# Get translations for the haupt + compound
+python translate.py -src "../wiktionary/s_length_haupt.src" -tgt "../wiktionary/s_length_lieblings.src" \
+                    -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
+                    -output "${PRD_FILENAME}_length_haupt_${step}.prd" --seed 1
+wait
+# Get translations for the not + compound
+python translate.py -src "../wiktionary/s_length_not.src" -tgt "../wiktionary/s_length_lieblings.src" \
+                    -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
+                    -output "${PRD_FILENAME}_length_not_${step}.prd" --seed 1
+wait
+# Get translations for the see + compound
+python translate.py -src "../wiktionary/s_length_see.src" -tgt "../wiktionary/s_length_lieblings.src" \
+                    -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
+                    -output "${PRD_FILENAME}_length_see_${step}.prd" --seed 1
+wait
+# Get translations for the lieblings + compound
+python translate.py -src "../wiktionary/s_length_lieblings.src" -tgt "../wiktionary/s_length_lieblings.src" \
+                    -model "${MODEL_FILENAME}_step_${sum}.pt" -gpu 0 -batch_size 20 -beam_size 5 -n_best 1 \
+                    -output "${PRD_FILENAME}_length_lieblings_${step}.prd" --seed 1
+wait
